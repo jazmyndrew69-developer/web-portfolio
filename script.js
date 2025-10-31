f4c84c7da194f47adf8fe6b8ce2cfdfc27f053e6
Jazmyn Drew <jazmyndrew69@gmail.com>
2025-10-31 16:16:46 +0530

    Fix file linking and folder structure

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const toggleBtn = document.getElementById("toggle-btn");
 const toggleWord = document.getElementById("toggle-word");
 
-let isDeveloper = true;
+let showingDeveloper = true;
 
 toggleBtn.addEventListener("click", () => {
-  isDeveloper = !isDeveloper;
-  toggleWord.textContent = isDeveloper ? "Developer" : "Designer";
-  toggleBtn.textContent = isDeveloper ? "Switch to Designer" : "Switch to Developer";
+  if (showingDeveloper) {
+    toggleWord.textContent = "Designer";
+    toggleBtn.textContent = "Switch to Developer";
+  } else {
+    toggleWord.textContent = "Developer";
+    toggleBtn.textContent = "Switch to Designer";
+  }
+  showingDeveloper = !showingDeveloper;
 });
 
 
